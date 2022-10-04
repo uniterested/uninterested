c43e3c344ccbabfb982f7ad471e92cee631fde8c
aslamtoolyt <110217093+aslamtoolyt@users.noreply.github.com>
2022-10-04 16:00:07 +0530

    hey
    hey

--- uninterested/lib/home/dashboard.dart
+++ uninterested/lib/home/dashboard.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
-class DashBoard extends StatelessWidget {
+class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  File? _image;
+  Future getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image == null) return;
+    final imageTemporary = File(image.path);
+    setState(() {
+      _image = imageTemporary;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Text('data'),
-        ],
+      appBar: AppBar(
+        title: const Text('Unintrested'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+                'https://media.gettyimages.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805'),
+            SizedBox(),
+            TextButton(
+              onPressed: () {},
+              child: Text('Gallery'),
+            ),
+            TextButton(
+                onPressed: () {
+                  getImage();
+                },
+                child: const Text('OpenCam'))
+          ],
+        ),
       ),
     );
   }
