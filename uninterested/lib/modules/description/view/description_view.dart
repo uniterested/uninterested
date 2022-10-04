c217caf9118eca01aa809f03b88d94f96fff796d
mds115577 <70107377+mds115577@users.noreply.github.com>
2022-10-04 19:22:06 +0530

    description started

---
+++ uninterested/lib/modules/description/view/description_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uninterested/modules/description/controller/decription_controller.dart';
+
+class DescripitonView extends GetView<DescriptionController> {
+  const DescripitonView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
