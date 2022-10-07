5ef8702229430f5d0fe8e0fa2a9523a4f3833d24
aslamtoolyt <110217093+aslamtoolyt@users.noreply.github.com>
2022-10-07 10:10:21 +0530

    home_screen

--- uninterested/lib/home/dashboard.dart
+++ uninterested/lib/home/dashboard.dart
@@ -23,25 +23,215 @@ class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('sdfsa'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.network(
-                'https://media.gettyimages.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805'),
-            SizedBox(),
-            TextButton(
-              onPressed: () {},
-              child: Text('Gallery'),
+      // body: Center(
+      //   child: Column(
+      //     children: [
+      // Image.network(
+      //     'https://media.gettyimages.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805'),
+      //       SizedBox(),
+      //       TextButton(
+      //         onPressed: () {},
+      //         child: Text('Gallery'),
+      //       ),
+      //       TextButton(
+      //           onPressed: () {
+      //             getImage();
+      //           },
+      //           child: const Text('OpenCam'))
+      //     ],
+      //   ),
+      // ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SafeArea(
+          child: Column(children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 350,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(227, 208, 224, 187),
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(120),
+                              bottomRight: Radius.circular(120))),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 18.0, left: 16),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 42,
+                            child: Icon(Icons.person),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 30.0, left: 16),
+                                child: Text(
+                                  "Hi... Aslam",
+                                  style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10.0, right: 10, top: 17, bottom: 12),
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: 180,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Center(
+                                child: Text(
+                                    'Cardea Health Care Solutions')), //book advertaisment
+                          ),
+                          Positioned(
+                            top: 9,
+                            left: 1,
+                            child: Container(
+                              height: 160,
+                              width: 8,
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Card(
+                            elevation: 0,
+                            child: Container(
+                              height: 190,
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.camera,
+                                  size: 120,
+                                ),
+                                onPressed: () {
+                                  getImage();
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Card(
+                            elevation: 0,
+                            child: Container(
+                              height: 190,
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Image.network(
+                                'https://media.gettyimages.com/photos/modern-laptop-with-empty-screen-on-white-background-mockup-design-picture-id1182241805',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Card(
+                            elevation: 0,
+                            child: Container(
+                              height: 190,
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Card(
+                            elevation: 0,
+                            child: Container(
+                              height: 190,
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Card(
+                            elevation: 0,
+                            child: Container(
+                              height: 190,
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Card(
+                            elevation: 0,
+                            child: Container(
+                              height: 190,
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: Colors.black12,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
             ),
-            TextButton(
-                onPressed: () {
-                  getImage();
-                },
-                child: const Text('OpenCam'))
-          ],
+          ]),
         ),
       ),
     );
