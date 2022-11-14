191c4e03b2a7432961650207066411ed1656d8b3
aslamtoolyt <110217093+aslamtoolyt@users.noreply.github.com>
2022-11-14 17:37:20 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uninterested/home/dashboard.dart';
 import 'package:uninterested/modules/description/view/description_view.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const DashBoardScreen(),
+      home: DescripitonView(),
     );
   }
 }
