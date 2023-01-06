488d7e97616a7890e39c7fc62291863a416a0c09
aslamtoolyt <110217093+aslamtoolyt@users.noreply.github.com>
2023-01-06 21:56:41 +0530

    '6-1-2023'
    otp screen adding flexible

--- lib/login/otp_screen.dart
+++ lib/login/otp_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:uninterested/utilities/AppColor.dart';
@@ -7,7 +6,6 @@ import 'package:uninterested/utilities/app_back_button.dart';
 import 'package:uninterested/utilities/app_leading_app_bar.dart';
 import 'package:uninterested/utilities/app_text_fitted_elevated_button.dart';
 import 'package:uninterested/utilities/formatter/otp_input.dart';
-
 
 class OtpScreen extends StatefulWidget {
   static String route = '/verify_otp';
@@ -21,33 +19,26 @@ class _OtpScreenState extends State<OtpScreen> {
   // late TextEditingController _otpController;
   bool isOtpFormatError = false;
   @override
-  
- 
-
   Widget build(BuildContext context) {
-    
-      return Scaffold(
-        backgroundColor: AppColorPallet.white,
-        appBar: AppLeadingAppBar(
-          background: AppColorPallet.white,
-          leading: AppBackButton(),
-          title: "Phone Verification",
-          
-        ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
+    return Scaffold(
+      backgroundColor: AppColorPallet.white,
+      appBar: AppLeadingAppBar(
+        background: AppColorPallet.white,
+        leading: AppBackButton(),
+        title: "Phone Verification",
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
               children: [
-                
-                Padding(
-                  padding: const EdgeInsets.only(top: 149),
-                  child: Text(
-                    "4 digit OTP has been sent to your no",
-                    style:
-                        AppTextStyle.sfpro(fontWeight: FontWeight.w400,
-                        fontSize: 17),
-                  ),
+                Expanded(
+                  child: Container(),
+                ),
+                Text(
+                  "4 digit OTP has been sent to your no",
+                  style: AppTextStyle.sfpro(
+                      fontWeight: FontWeight.w400, fontSize: 17),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -59,22 +50,20 @@ class _OtpScreenState extends State<OtpScreen> {
                         borderRadius: BorderRadius.circular(7),
                         child: Container(
                           decoration: BoxDecoration(
-                          border: Border.all(color: AppColorPallet.txt_fld_border),
-                            borderRadius: BorderRadius.circular(7),
-                             color: AppColorPallet.white
-                            
-                          ),
-                          
+                              border: Border.all(
+                                  color: AppColorPallet.txt_fld_border),
+                              borderRadius: BorderRadius.circular(7),
+                              color: AppColorPallet.white),
+
                           height: 46,
                           width: 200,
-                          
+
                           // width: MediaQuery.of(context).size.width * 4 / 5,
                           child: Center(
                             child: TextField(
-                              
                               // controller: _otpController,
                               autofocus: true,
-                              
+
                               style: AppTextStyle.sfpro(
                                 fontSize: 17,
                                 fontWeight: FontWeight.w600,
@@ -87,16 +76,14 @@ class _OtpScreenState extends State<OtpScreen> {
                               keyboardType: TextInputType.number,
                               textAlign: TextAlign.center,
                               decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(vertical: 2),
+                                contentPadding:
+                                    EdgeInsets.symmetric(vertical: 2),
                                 hintText: "Enter OTP",
                                 hintStyle: AppTextStyle.sfpro(
                                   fontSize: 17,
                                   fontWeight: FontWeight.w600,
                                   color: AppColorPallet.hint,
-                      
-                                  
                                 ),
-                              
                                 focusedBorder: OutlineInputBorder(
                                   borderSide: BorderSide(
                                     color: Colors.transparent,
@@ -106,7 +93,6 @@ class _OtpScreenState extends State<OtpScreen> {
                                   borderRadius: BorderRadius.circular(7),
                                   borderSide: const BorderSide(
                                       color: Colors.transparent),
-                                                
                                 ),
                               ),
                             ),
@@ -127,20 +113,17 @@ class _OtpScreenState extends State<OtpScreen> {
                   ],
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 40,right: 40, top: 48),
+                  padding: const EdgeInsets.only(left: 40, right: 40, top: 48),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
                         "Didn’t recive the OTP ?",
                         style: AppTextStyle.sfpro(
-                          fontSize: 17,
-                            fontWeight: FontWeight.w400),
+                            fontSize: 17, fontWeight: FontWeight.w400),
                       ),
                       TextButton(
-                        onPressed: () {
-                          
-                        },
+                        onPressed: () {},
                         child: Text(
                           'Resend OTP',
                           style: AppTextStyle.sfpro(
@@ -150,96 +133,82 @@ class _OtpScreenState extends State<OtpScreen> {
                     ],
                   ),
                 ),
-                  
-                 Padding(
-                  padding: EdgeInsets.only(left: 40,right: 33,top: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: 40, right: 33, top: 20),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
                         "Automatically detect the OTP ?",
                         style: AppTextStyle.sfpro(
-                          fontSize: 17,
-                            fontWeight: FontWeight.w400),
+                            fontSize: 17, fontWeight: FontWeight.w400),
                       ),
-                      
                       TextButton(
-                        onPressed: () {
-                          
-                        },
+                        onPressed: () {},
                         child: Text(
                           'Yes',
                           style: AppTextStyle.sfpro(
-                              fontWeight: FontWeight.w500, fontSize: 17,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17,
                               color: AppColorPallet.pink2),
                         ),
                       )
                     ],
                   ),
                 ),
-                  
-                SizedBox(
-                  height: 193,
+                Flexible(
+                  child: SizedBox(
+                    height: 190,
+                  ),
                 ),
                 SizedBox(
                   width: 178,
                   height: 48,
                   child: AppTextFittedElevatedButton(
                     elevation: 3,
-                    onPressed: () {
-                    
-                    },
+                    onPressed: () {},
                     child: Text(
                       "Verify",
-                     style: AppTextStyle.sfpro(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: AppColorPallet.white
-                     ),
+                      style: AppTextStyle.sfpro(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: AppColorPallet.white),
                     ),
                   ),
                 ),
-                
                 Padding(
                   padding: const EdgeInsets.only(
-                    top:56,
-                    left: 22.0,
-                    right: 22,
-                    bottom: 63
-                  ),
+                      top: 56, left: 22.0, right: 22, bottom: 63),
                   child: GestureDetector(
-                    onTap: ()  {
-                      
-                    },
+                    onTap: () {},
                     child: RichText(
                       textAlign: TextAlign.center,
                       text: TextSpan(
                         children: [
                           TextSpan(
-                              text: "By continuing you acknowledge that you have read and understood and agree to our  ",
+                              text:
+                                  "By continuing you acknowledge that you have read and understood and agree to our  ",
                               style: AppTextStyle.sfpro(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                color:AppColorPallet.grey2
-                              )),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  color: AppColorPallet.grey2)),
                           TextSpan(
                             text: "Terms of service ",
                             style: AppTextStyle.sfpro(
-                              fontSize: 12,
+                                fontSize: 12,
                                 fontWeight: FontWeight.w500,
                                 color: AppColorPallet.blue2),
                           ),
                           TextSpan(
                               text: "and ",
                               style: AppTextStyle.sfpro(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                color: AppColorPallet.grey2
-                              )),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  color: AppColorPallet.grey2)),
                           TextSpan(
                             text: "Privacy Policy",
                             style: AppTextStyle.sfpro(
-                              fontSize: 12,
+                                fontSize: 12,
                                 fontWeight: FontWeight.w500,
                                 color: AppColorPallet.blue2),
                           ),
@@ -248,12 +217,11 @@ class _OtpScreenState extends State<OtpScreen> {
                     ),
                   ),
                 ),
-               
               ],
             ),
-          ),
+          ],
         ),
-      
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'SFPro',
         // primarySwatch: Colors.blue,
       ),
-      home:   DashBoardScreen(),
+      home: OtpScreen(),
     );
   }
 }
