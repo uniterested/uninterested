903c9b28193cdae1e284d72b23cd50b885d6d404
aslamtoolyt <110217093+aslamtoolyt@users.noreply.github.com>
2023-01-11 11:18:58 +0530

    cart

---
+++ lib/home/commonContainer.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:uninterested/utilities/AppColor.dart';
+import 'package:uninterested/utilities/app_back_button.dart';
+import 'package:uninterested/utilities/app_leading_app_bar.dart';
+import 'package:uninterested/utilities/app_text_fitted_elevated_button.dart';
+
+class CommonContainer extends StatelessWidget {
+  const CommonContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColorPallet.white,
+        appBar: AppLeadingAppBar(
+          leading: AppBackButton(),
+          title: "Cart",
+          background: AppColorPallet.white,
+        ),
+        body: CommonContainer2());
+  }
+
+  // Container buutoncontainer(String tittle) {
+  //   return Container(
+  //     decoration: BoxDecoration(
+  //         borderRadius: BorderRadius.circular(20), color: Colors.white),
+  //     width: 190,
+  //     height: 80,
+  //     child: TextButton(onPressed: () {}, child: Text(tittle)),
+  //   );
+  // }
+
+  final kBlack = Colors.black;
+  final kgrey = Colors.grey;
+  final kred = Colors.red;
+}
+
+class CommonContainer2 extends StatelessWidget {
+  const CommonContainer2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            height: 120,
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Image.asset("assets/profile1.png", fit: BoxFit.cover),
+                ),
+                Container(
+                  width: 300,
+                  height: 80,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            width: 250,
+                            height: 70,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Lorem ipsum", textAlign: TextAlign.start),
+                                Text(
+                                    "LETHER  HAND MAID POASAS AJJGGKGG BFDKDFHDFNFND")
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.abc))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Text(
+            "LETHER  HAND MAID POASAS AJJGGKGG BFDKDFHDFNFND",
+            maxLines: 2,
+            textAlign: TextAlign.start,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                AppTextFittedElevatedButton(
+                    onPressed: () {}, child: Text("Add to Cart")),
+                AppTextFittedElevatedButton(
+                    onPressed: () {}, child: Text("Buy now"))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/utilities/AppIcons.dart
+++ lib/utilities/AppIcons.dart
@@ -1,19 +1,20 @@
 class AppLeadingIcons {
-  static const String back_button = _iconPath + 'back_button.svg';
   static const String _iconPath = 'assets/';
+  static const String back_button = _iconPath + 'back_button.svg';
+
   static const String menu = _iconPath + 'menu.png';
   static const String profile = _iconPath + 'profile.png';
   static const String notification_icon = _iconPath + 'notification_icon.png';
-  static const String share=_iconPath+'share.svg';
-  static const String back=_iconPath+'back.svg';
-  static const String backbtn=_iconPath+'backbtn.svg';
-  static const String forwardbtn=_iconPath+'forbtn.svg';
-  static const String edit=_iconPath+'edit.svg';
-  static const String arrow_down=_iconPath+'arrow_down.svg';
-  static const String flag=_iconPath+'flag1.svg';
-  static const String plan=_iconPath+'plan.svg';
-  static const String profile_icon=_iconPath+'profile_icon.svg';
-  static const String Payment=_iconPath+'payment.svg';
-  static const String about=_iconPath+'about_us.svg';
-  static const String log_out=_iconPath+'log_out.svg';
+  static const String share = _iconPath + 'share.svg';
+  static const String back = _iconPath + 'back.svg';
+  static const String backbtn = _iconPath + 'backbtn.svg';
+  static const String forwardbtn = _iconPath + 'forbtn.svg';
+  static const String edit = _iconPath + 'edit.svg';
+  static const String arrow_down = _iconPath + 'arrow_down.svg';
+  static const String flag = _iconPath + 'flag1.svg';
+  static const String plan = _iconPath + 'plan.svg';
+  static const String profile_icon = _iconPath + 'profile_icon.svg';
+  static const String Payment = _iconPath + 'payment.svg';
+  static const String about = _iconPath + 'about_us.svg';
+  static const String log_out = _iconPath + 'log_out.svg';
 }
